7668def06d71753ed1e72d53fccc5c5ba1b14f31
Alesia-V175 <vaivasilisa@gmail.com>
2022-08-10 13:20:33 +0300

    feat: add morse task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let string = "";
+    let arr = [];
+    const strSplit = expr.split("**********");
+
+   for (let i in strSplit) {
+       string = strSplit[i];
+
+       while (true) {
+           if (string.length >= 10) {
+               arr.push(string.slice(0, 10));
+               string = string.slice(10);
+           } else {
+               arr.push(string);
+               break;
+           }
+       }
+
+       for (let j in arr) {
+           arr[j] = arr[j].replace(/(10)/gm, ".");
+           arr[j] = arr[j].replace(/(11)/gm, "-");
+           arr[j] = arr[j].replace(/\d+/gm, "");
+           arr[j] = MORSE_TABLE[arr[j]];
+       }
+
+       strSplit[i] = arr.join("");
+       arr = [];
+   }
+   return strSplit.join(" ")
 }
 
 module.exports = {
